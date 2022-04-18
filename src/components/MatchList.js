29688e5706c70c6d872620be111beb8aa50c9b3c
trishnarjun kunamalla <trishnarjun.k@gmail.com>
2022-04-18 12:45:26 -0400

    tourney-matches

---
+++ src/components/MatchList.js
@@ -0,0 +1,20 @@
+import matchData from '../data/matchData';
+import Match from "./Match";
+
+function MatchList() {
+  const matches = matchData.map((match) => {
+    return (
+      <Match key={match.matchNumber} players={match.players} winner={match.winner} scoreDifference={match.scoreDifference} />
+    )
+  })
+  return (
+    <section className="PlayerList MatchList">
+      <h1>Match list</h1>
+      {matches}
+
+    </section>
+  );
+  
+}
+
+export default MatchList;
---
+++ src/components/PlayerList.js
@@ -0,0 +1,26 @@
+import playerData from '../data/playerData';
+import matchData from '../data/matchData';
+import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
+import Player from "./Player";
+
+function PlayerList(props) {  
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  const players = parsedPlayerData.map(player => {
+    return (
+    <Player
+      key={player.gamerTag}
+      gamerTag={player.gamerTag} 
+      firstName={player.firstName} 
+      lastName={player.lastName} 
+      wins={player.wins} />)
+  });
+  return (
+    <section className="PlayerList">
+      <h1>Current participating players</h1>
+      {players}
+    </section>
+  );
+}
+
+export default PlayerList;
